3a130d362bcba108dfda8218c4e65f73c6de46c6
Ismatulla <ismatilloqoyliyev@gmail.com>
2023-05-26 08:26:35 +0200

    advanced-markdown-complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,146 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Container} from "react-bootstrap";
+import {Routes, Route, Navigate} from "react-router-dom";
+import NewNote from "./NewNote";
+import {NoteList} from "./NoteList";
+import useLocalStorage from "./useLocalStorage";
+import {useMemo} from "react";
+import {v4 as uuidv4} from "uuid";
+import NoteLayout from "./NoteLayout";
+import Note from "./Note";
+import EditNote from "./EditeNote";
 
-function App() {
-  const [count, setCount] = useState(0)
+export type Note = {
+  id: string;
+} & NoteData;
 
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
+export default function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("notes", []);
+  const [tags, setTags] = useLocalStorage<Tag[]>("tags", []);
+
+  const noteWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({tags, ...data}: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        {...data, id: uuidv4(), tagIds: tags.map((tag) => tag.id)},
+      ];
+    });
+  }
+
+  function onUpdateNote(id: string, {tags, ...data}: NoteData) {
+    setNotes((prevNotes) => {
+      return prevNotes.map((note) => {
+        if (note.id === id) {
+          return {
+            ...note,
+            ...data,
+            id: uuidv4(),
+            tagIds: tags.map((tag) => tag.id),
+          };
+        } else {
+          return note;
+        }
+      });
+    });
+  }
+
+  function onDeleteNote(id: string) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note) => note.id !== id);
+    });
+  }
+
+  function addTag(tag: Tag) {
+    setTags((prev) => [...prev, tag]);
+  }
+
+  function deleteTag(id: string) {
+    setTags((prevTags) => {
+      return prevTags.filter((tag) => tag.id !== id);
+    });
+  }
+
+  function updateTag(id: string, label: string) {
+    setTags((prevTags) => {
+      return prevTags.map((tag) => {
+        if (tag.id === id) {
+          return {...tag, label};
+        } else {
+          return tag;
+        }
+      });
+    });
+  }
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Container className="my-4">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <NoteList
+              notes={noteWithTags}
+              availableTags={tags}
+              onDeleteTag={deleteTag}
+              onUpdateTag={updateTag}
+            />
+          }
+        />
+        <Route
+          path="/new"
+          element={
+            <NewNote
+              onSubmit={onCreateNote}
+              onAddTag={addTag}
+              availableTags={tags}
+            />
+          }
+        />
 
-export default App
+        <Route path="/:id" element={<NoteLayout notes={noteWithTags} />}>
+          <Route index element={<Note onDelete={onDeleteNote} />} />
+          <Route
+            path="edit"
+            element={
+              <EditNote
+                onSubmit={onUpdateNote}
+                onAddTag={addTag}
+                availableTags={tags}
+              />
+            }
+          />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />}></Route>
+      </Routes>
+    </Container>
+  );
+}
---
+++ src/EditeNote.tsx
@@ -0,0 +1,24 @@
+import {NoteData, Tag} from "./App";
+import NoteForm from "./NoteForm";
+import { useNote } from "./NoteLayout";
+type EditNoteProps = {
+  onSubmit: (id: string, data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availableTags: Tag[];
+};
+
+function EditNote({onSubmit, onAddTag, availableTags}: EditNoteProps) {
+  const note = useNote();
+  return (
+    <div>
+      <h1>Edit Note</h1>
+      <NoteForm
+        onSubmit={(data) => onSubmit(note.id, data)}
+        onAddTag={onAddTag}
+        availableTags={availableTags}
+      />
+    </div>
+  );
+}
+
+export default EditNote;
